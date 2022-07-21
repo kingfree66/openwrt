4850fe6bab98eec82fd2a28b312965cb8a52c822
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2022-07-21 20:46:29 +1000

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
+#=============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
+
 
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+
 
 # 修改openwrt登陆地址,把下面的192.168.2.66修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.2.66/g' package/base-files/files/bin/config_generate
