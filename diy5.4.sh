d6f4f4a1f2eea162f07e3c84ab821158c67ac80e
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2022-11-06 20:21:43 +0800

    Update diy5.4.sh

--- diy5.4.sh
+++ diy5.4.sh
@@ -21,19 +21,35 @@ sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-King'" 
 sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-King'/g" ./package/base-files/files/bin/config_generate
 
 # Add a feed source
-sed -i '$a src-git helloworld https://github.com/fw876/helloworld' feeds.conf.default
-sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall;luci' feeds.conf.default
-sed -i '$a src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' feeds.conf.default
-sed -i '$a src-git diy https://github.com/firker/diy-ziyong' feeds.conf.default
-
+# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+git clone https://github.com/fw876/helloworld.git package/ssr
+git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
+git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+# git clone https://github.com/siropboy/sirpdboy-package package/sirpdboy-package
+git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
+sed -i 's/luci-lib-ipkg/luci-base/g' package/openwrt-bypass/luci-app-bypass/Makefile
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+# git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
+# git clone https://github.com/dwj0/luci-app-sms-tool.git package/luci-app-sms-tool
+# git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
+# git clone https://github.com/linkease/istore.git package/istore
+# sed -i 's/luci-lib-ipkg/luci-base/g' package/istore/luci/luci-app-store/Makefile
+# git clone https://github.com/linkease/istore-ui.git package/istore-ui
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-svn co https://github.com/Lienol/openwrt-package/trunk/luci-app-filebrowser package/luci-app-filebrowser
 git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
-git clone https://github.com/zxlhhyccc/luci-app-v2raya package/luci-app-v2raya
-git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
-git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
 
 #检索feeds
 ./scripts/feeds update -a
