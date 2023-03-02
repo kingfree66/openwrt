d7ef5f535580d19983a9e4aca89f0de87ee5fe26
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2023-03-01 17:51:47 -0800

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -15,6 +15,13 @@ sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Make
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
+# 修改openwrt登陆地址,把下面的192.168.2.66修改成你想要的就可以了
+sed -i 's/192.168.1.1/192.168.2.66/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把Unicorn修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-King'" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-King'/g" ./package/base-files/files/bin/config_generate
+
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
