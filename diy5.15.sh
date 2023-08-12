5e7384adbda08944654890bdf7c802daae87762d
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2023-08-12 02:02:42 +0000

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -38,7 +38,7 @@ src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityf
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
 git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
 git clone https://github.com/firkerword/smartdns.git package/smartdns
