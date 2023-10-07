12edfee25e8b1bbf4362a4d71e456ed867515535
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2023-10-07 12:24:19 +0000

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -8,8 +8,8 @@
 # Blog: https://p3terx.com
 #=============================================================
 
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
-sed -i 's/KERNEL_PATCHVER:=6.2/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=5.10/g' ./target/linux/x86/Makefile
 
 # 修改openwrt登陆地址,把下面的192.168.2.106修改成你想要的就可以了
 sed -i 's/192.168.1.1/192.168.2.106/g' package/base-files/files/bin/config_generate
