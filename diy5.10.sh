b772e88273ad2c091810bd0d4bfc93aa5b1aa09e
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2023-10-07 12:30:31 +0000

    Update diy5.10.sh

--- diy5.10.sh
+++ diy5.10.sh
@@ -53,7 +53,7 @@ git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git pac
 # git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
 git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
 # git clone https://github.com/gdy666/luci-app-lucky.git package/lucky
-# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
 # git clone https://github.com/dwj0/luci-app-sms-tool.git package/luci-app-sms-tool
 # git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 # git clone https://github.com/linkease/istore.git package/istore
