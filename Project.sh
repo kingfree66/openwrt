d6ef8636ecba49fefc1c6b84a81dc9e05e35e692
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2023-10-13 05:17:37 +0000

    Update Project.sh

--- Project.sh
+++ Project.sh
@@ -21,33 +21,41 @@ sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-King'" 
 sed -i "s/hostname='immortalwrt'/hostname='OpenWrt-King'/g" ./package/base-files/files/bin/config_generate
 
 # Add a feed source
-sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
-sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-# git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
-git clone https://github.com/fw876/helloworld.git package/ssr
+git clone https://github.com/jerrykuku/lua-maxminddb.git  package/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr.git  package/luci-app-vssr
+git clone https://github.com/fw876/helloworld.git package/ssr
+# git clone https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
-# git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
-# git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
+git clone https://github.com/gngpp/luci-theme-neobird.git package/luci-theme-neobird
+git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
+src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git
+# find ./ | grep Makefile | grep mosdns | xargs rm -f
+# git clone https://github.com/firkerword/openwrt-mos.git package/openwrt-mos
+# git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+# find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
-git clone https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
-git clone https://github.com/linkease/istore.git package/istore
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/linkease/istore-ui.git package/istore-ui
-git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
-git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
-# svn co https://github.com/coolsnowwolf/packages/trunk/net/redsocks2 package/redsocks2
-# svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
+git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-smartdns
+git clone https://github.com/firkerword/smartdns.git package/smartdns
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
+find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
+find ./ | grep Makefile | grep mosdns | xargs rm -f
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+git clone https://github.com/sbwml/luci-app-dae package/dae
+git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
+git clone -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush.git package/luci-app-wechatpush
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-bypass package/luci-app-bypass
+# git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
+# git clone https://github.com/linkease/istore.git package/istore
+# git clone https://github.com/linkease/istore-ui.git package/istore-ui
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-wrtbwmon package/luci-app-wrtbwmon
-svn co https://github.com/kiddin9/openwrt-packages/trunk/wrtbwmon package/wrtbwmon
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
