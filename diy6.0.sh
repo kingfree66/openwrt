50f89ca9a80e70ab86b1d3567b9cc830e5596c4a
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2024-02-01 18:10:03 -0800

    Update diy6.0.sh

--- diy6.0.sh
+++ diy6.0.sh
@@ -18,6 +18,13 @@ sed -i 's/192.168.1.1/192.168.2.106/g' package/base-files/files/bin/config_gener
 sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-King'" package/lean/default-settings/files/zzz-default-settings
 sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-King'/g" ./package/base-files/files/bin/config_generate
 
+# 修改openwrt登陆地址,把下面的192.168.2.106修改成你想要的就可以了
+sed -i 's/192.168.1.1/192.168.2.106/g' package/base-files/files/bin/config_generate
+
+# 修改主机名字，把Unicorn修改你喜欢的就行（不能纯数字或者使用中文）
+sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-King'" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-King'/g" ./package/base-files/files/bin/config_generate
+
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
@@ -37,6 +44,7 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 # git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 # git clone https://github.com/dwj0/luci-app-sms-tool.git package/luci-app-sms-tool
 # git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
@@ -45,12 +53,13 @@ git clone https://github.com/firkerword/luci-app-smartdns.git package/luci-app-s
 git clone https://github.com/firkerword/smartdns.git package/smartdns
 # git clone https://github.com/firkerword/luci-app-mosdns.git package/OpenWrt-mosdns
 # git clone https://github.com/immortalwrt/homeproxy.git package/luci-app-homeproxy
-# git clone https://github.com/linkease/istore.git package/istore
-# git clone https://github.com/linkease/istore-ui.git package/istore-ui
+git clone https://github.com/linkease/istore.git package/istore
+git clone https://github.com/linkease/istore-ui.git package/istore-ui
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
 # svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+https://github.com/OpenWrt-Actions/luci-app-vssr.git
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 
