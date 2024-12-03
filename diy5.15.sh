c0959bc73abbefa031ac2e65428b63968891acba
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2024-12-02 23:45:54 -0800

    Update diy5.15.sh

--- diy5.15.sh
+++ diy5.15.sh
@@ -28,15 +28,8 @@ function drop_package(){
 
 rm -rf package/custom; mkdir package/custom
 
-# 修改openwrt登陆地址,把下面的192.168.2.106修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.2.106/g' package/base-files/files/bin/config_generate
-
-# 修改主机名字，把Unicorn修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-King'" package/lean/default-settings/files/zzz-default-settings
-sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-King'/g" ./package/base-files/files/bin/config_generate
-
 # Add a feed source
-git clone https://github.com/fw876/helloworld.git package/ssr
+# git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
@@ -48,16 +41,16 @@ git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme
 # find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
-git clone https://github.com/linkease/istore.git package/istore
-git clone https://github.com/linkease/istore-ui.git package/istore-ui
+# git clone -b luci-smartdns-new-version https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+# git clone https://github.com/firkerword/luci-app-mosdns.git package/mosdns
+# git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 find ./ | grep Makefile | grep v2ray-geodata | xargs rm -f
 find ./ | grep Makefile | grep mosdns | xargs rm -f
 git clone https://github.com/sbwml/luci-app-mosdns -b v5-lua package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 git clone https://github.com/firkerword/luci-app-serverchan.git package/luci-app-serverchan
-# git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
-git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+# git clone https://github.com/sirpdboy/luci-app-lucky.git package/lucky
+git clone https://github.com/firkerword/luci-app-lucky.git package/lucky
 chmod 755 ./package/lucky/luci-app-lucky/root/usr/bin/luckyarch
 git clone https://github.com/liudf0716/luci-app-apfree-wifidog.git package/wifidog
 git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
