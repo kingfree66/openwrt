21c615e9a4e37c7bd44ec08a73ac39b3d4f8fbe6
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2025-07-24 19:31:38 +0800

    Update diy6.6.sh

--- diy6.6.sh
+++ diy6.6.sh
@@ -10,7 +10,7 @@
 
 sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.6/g' ./target/linux/x86/Makefile
-sed -i '/openwrt-24.01/d' feeds.conf.default
+sed -i '/openwrt-24.10/d' feeds.conf.default
 sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
