430b06c865bfb32a9127bd08a6383548f8ab2785
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2025-07-27 07:34:39 +0800

    Update immortalwrt2.sh

--- immortalwrt2.sh
+++ immortalwrt2.sh
@@ -18,8 +18,8 @@
 sed -i 's/192.168.1.1/192.168.2.106/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把Unicorn修改你喜欢的就行（不能纯数字或者使用中文）
-# sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-King'" package/lean/default-settings/files/zzz-default-settings
-# sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-King'/g" ./package/base-files/files/bin/config_generate
+sed -i "/uci commit system/i\uci set system.@system[0].hostname='OpenWrt-King'" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-King'/g" ./package/base-files/files/bin/config_generate
 
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 rm -rf ./package/diy-ziyong/theme
