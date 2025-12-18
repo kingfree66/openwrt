0fb13f4af2d29a5ff95fb24358c98a4ea02688a3
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2025-12-18 04:48:57 +0000

    Update diy-part3.sh

--- diy-part3.sh
+++ diy-part3.sh
@@ -36,3 +36,10 @@ rm -rf ./package/ssr/v2ray-geodata
 # svn co https://github.com/Lienol/openwrt-packages/trunk/net/https-dns-proxy feeds/packages/net/https-dns-proxy
 # rm -rf feeds/packages/lang/golang
 # git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+mkdir -p files/etc/config
+mkdir -p files/etc/config
+cat > files/etc/config/luci << 'EOF'
+config core 'main'
+    option lang 'auto'
+    option mediaurlbase '/luci-static/kucat'
+EOF
