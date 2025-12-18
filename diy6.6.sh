f70d8f4519e6997882413f76843325f4bde4001b
kingfree66 <63577130+kingfree66@users.noreply.github.com>
2025-12-18 01:29:04 +0000

    Update diy6.6.sh

--- diy6.6.sh
+++ diy6.6.sh
@@ -39,6 +39,7 @@ sed -i "s/hostname='OpenWrt'/hostname='OpenWrt-King'/g" ./package/base-files/fil
 git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone -b 18.06 https://github.com/garypang13/luci-theme-edge.git package/luci-theme-edge
+git clone https://github.com/sirpdboy/luci-theme-kucat.git
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/gngpp/luci-theme-neobird.git package/luci-theme-neobird
 git clone https://github.com/sirpdboy/luci-theme-opentopd.git package/luci-theme-opentopd
